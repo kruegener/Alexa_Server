346c4c1737bb9c538271d1ab78a513dac829eb19
kruegener <moritz.kruegener@gmail.com>
2017-04-10 18:04:26 +0200

    admin menu cleanup

--- AlexaHandler/admin.py
+++ AlexaHandler/admin.py
@@ -6,9 +6,9 @@ from .models import *
 
 admin.site.register(Person)
 admin.site.register(File)
-admin.site.register(ClientSession)
-admin.site.register(NodeFlow)
-admin.site.register(Node)
-admin.site.register(SessionVar)
+#admin.site.register(ClientSession)
+#admin.site.register(NodeFlow)
+#admin.site.register(Node)
+#admin.site.register(SessionVar)
 admin.site.register(BlockChainModel)
 admin.site.register(BlockModel)
