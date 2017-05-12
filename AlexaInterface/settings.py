23c69cf7f24a75c2d46a426450233575ab63b82f
kruegener <moritz.kruegener@gmail.com>
2017-05-12 16:56:43 +0200

    volatile memory cache works

--- AlexaInterface/settings.py
+++ AlexaInterface/settings.py
@@ -138,9 +138,15 @@ STATICFILES_DIRS = [
 ]
 
 CACHES = {
+    # DB Cache Backend
+    # 'default': {
+    #     'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+    #     'LOCATION': 'cache_table',
+    # }
+    # Memcached Backend
     'default': {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'cache_table',
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
     }
 }
 
